81022b107bfc5dabf50705b9a97e2bd0076200a9
moretti-matteo <matteomoretti2000@hotmail.it>
2022-05-04 11:30:45 +0200

    first commit

---
+++ snack-1/js/script.js
@@ -0,0 +1,10 @@
+const num1 = prompt("Scrivi il primo numero");
+const num2 = prompt("Scrivi il secondo numero");
+
+if (num1 > num2) {
+    console.log(num1);
+} else if (num1 < num2) {
+    console.log(num2);
+} else {
+    console.log("i due numeri sono uguali");
+}
